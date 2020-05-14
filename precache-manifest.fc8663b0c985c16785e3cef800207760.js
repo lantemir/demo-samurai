faae9f58cd5d7e833d9bdcb665edb77537c669cb
lantemir <42696579+lantemir@users.noreply.github.com>
2020-05-14 23:59:20 +0600

    Add files via upload

---
+++ precache-manifest.fc8663b0c985c16785e3cef800207760.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94878f178147026164a2fa41bf0b8cf8",
+    "url": "/demo-samurai/index.html"
+  },
+  {
+    "revision": "b93120f363af0a537542",
+    "url": "/demo-samurai/static/css/3.9665a91b.chunk.css"
+  },
+  {
+    "revision": "90ea30b81a5fbeef3761",
+    "url": "/demo-samurai/static/css/4.a726c447.chunk.css"
+  },
+  {
+    "revision": "793c063a0ebf6c8a1100",
+    "url": "/demo-samurai/static/css/main.188468f1.chunk.css"
+  },
+  {
+    "revision": "02ceb4e2189dac52c024",
+    "url": "/demo-samurai/static/js/2.e3388a44.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/demo-samurai/static/js/2.e3388a44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b93120f363af0a537542",
+    "url": "/demo-samurai/static/js/3.51a220f6.chunk.js"
+  },
+  {
+    "revision": "90ea30b81a5fbeef3761",
+    "url": "/demo-samurai/static/js/4.851aae7a.chunk.js"
+  },
+  {
+    "revision": "793c063a0ebf6c8a1100",
+    "url": "/demo-samurai/static/js/main.759f6723.chunk.js"
+  },
+  {
+    "revision": "0f59de5efb353798eafc",
+    "url": "/demo-samurai/static/js/runtime-main.029d2a9c.js"
+  },
+  {
+    "revision": "8c2f01dcc6ffe4114b04e496efee4b74",
+    "url": "/demo-samurai/static/media/preloader.8c2f01dc.svg"
+  },
+  {
+    "revision": "6db09181ed93f7e8f2dac478623e3629",
+    "url": "/demo-samurai/static/media/user.6db09181.png"
+  }
+]);
